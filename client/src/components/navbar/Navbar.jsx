b79fa437c0408d9cb43dc43ed3715c89613de8e0
maitript <93444567+maitript@users.noreply.github.com>
2023-07-04 23:05:06 +0530

    done

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import "./navbar.css"
+import {Link} from "react-router-dom"
+import { AuthContext } from "../../context/AuthContext";
+import { useContext } from "react";
+
+const Navbar = () => {
+  const {user} = useContext(AuthContext);
+
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+      <Link to = "/" style = {{color:"inherit",textDecoration:"none"}}>
+      <span className="logo">Holiday Booking</span>
+      </Link>
+        {user ? user.username : <div className="navItems">
+          <Link to ="/login"><button className="navButton">Register</button></Link>
+          <Link to ="/login"><button className="navButton">Login</button></Link>
+        </div> }
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
